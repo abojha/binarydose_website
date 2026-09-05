f28b60b002a15b2beab11cc265d99cf56225b14c
Abhay Ojha <abhayojha0012@gmail.com>
2026-09-05 19:28:22 +0530

    feat(seo): redesign master and algodose OG banners; remove personal image

--- src/pages/algodose.jsx
+++ src/pages/algodose.jsx
@@ -14,8 +14,13 @@ export default function AlgoDosePage() {
         <meta property="og:title" content={pageTitle} />
         <meta property="og:description" content={pageDescription} />
         <meta property="og:type" content="website" />
+        <meta property="og:image" content="https://binarydose.in/img/algodose-og.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={pageTitle} />
         <meta name="twitter:description" content={pageDescription} />
+        <meta name="twitter:image" content="https://binarydose.in/img/algodose-og.png" />
       </Head>
       <AlgoDoseShell />
     </Layout>
